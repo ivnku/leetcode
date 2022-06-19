3ab53da352872614a0a3c823f53a19e60b8a7c74
ivnku <ibortsov14@gmail.com>
2022-06-20 00:28:29 +0300

    Solve the problem

--- tasks/add_two_numbers_2/add_numbers.go
+++ tasks/add_two_numbers_2/add_numbers.go
@@ -5,6 +5,38 @@ type ListNode struct {
 	Next *ListNode
 }
 
+func getVal(ln *ListNode) int {
+	if ln == nil {
+		return 0
+	} else {
+		return ln.Val
+	}
+}
+
+func getNext(ln *ListNode) *ListNode {
+	if ln == nil || ln.Next == nil {
+		return &ListNode{}
+	} else {
+		return ln.Next
+	}
+}
+
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return &ListNode{}
+	result := &ListNode{}
+	carry := 0
+	current := result
+	for {
+		nodesSum := getVal(l1) + getVal(l2) + carry
+		remainder := nodesSum % 10
+		carry = nodesSum / 10
+		current.Val = remainder
+		if l1.Next == nil && l2.Next == nil && carry == 0 {
+			break
+		} else {
+			l1, l2 = getNext(l1), getNext(l2)
+			current.Next = &ListNode{}
+			current = current.Next
+		}
+	}
+	return result
 }
